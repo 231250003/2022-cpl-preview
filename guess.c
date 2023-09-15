14e33471827600aa21c00785b78e1637c156c57c
crz <231250003@smail.nju.edu.cn>
2023-09-15 15:54:22 +0800

    renewing guess

--- guess.c
+++ guess.c
@@ -4,10 +4,38 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
+    int high=100;
+    int ans=30,input;
+    int step=7;
+    srand((unsigned int)time(NULL));
+    ans=rand()%100;
+    int t;
+    for(int i=1;i<=7;i++)
+    {
+        printf("you have %d tries",step);
+        scanf("%d",&input);
+        if(input>ans)
+        {
+            if(input>=t) printf("idiot\n");
+            printf("lesser\n");
+            step--;
+            t=input;
+        }
+        if(input<ans)
+        {
+            if(input<=t) printf("idiot\n");
+            printf("greater\n");
+            step--;
+            t=input;
+        }
+        if(input==ans)
+        {
+            printf("right\n");
+            printf("you use %d tries",i);
+            break;
+        }
+        if(step==0) printf("nice tries");
+    }
 
-    scanf("%d%d",&a,&b);
-    c=a*b;
-    printf("%d",c);
     return 0;
 }
